a1f3d3d6e43135c477dd13a260b8dea9fe61dbb6
parag <pal.parag257@gmail.com>
2021-08-13 17:11:56 +0530

    Added tilemap + level1

--- Platformer2D/Player.gd
+++ Platformer2D/Player.gd
@@ -1,17 +1,29 @@
 extends KinematicBody2D
 
+const speed = 200
+const maxVelocity = 300
+const gravity = 25
+const jump = 900
 var velocity = Vector2(0,0)
-
+	
 func _physics_process(dt):
+
 	if Input.is_action_pressed("right"):
-		velocity.x = 100
+		velocity.x = speed
+		$Sprite.flip_h = false
+		$Sprite.play("walk")
 	elif Input.is_action_pressed("left"):
-		velocity.x = -100
-	elif Input.is_action_pressed("up"):
-		velocity.y = -100
-	elif Input.is_action_pressed("down"):
-		velocity.y = 100
-	#print(dt)
-	move_and_slide(velocity)
+		velocity.x = -speed
+		$Sprite.flip_h = true
+		$Sprite.play("walk")
+	else:
+		$Sprite.play("idle")
+	
+	if not is_on_floor():
+		$Sprite.play("air")
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = -jump
+	velocity.y += gravity
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
 	velocity.x = lerp(velocity.x, 0, 0.1)
-	velocity.y = lerp(velocity.y, 0, 0.1)
